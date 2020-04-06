24049572b5eb060867471103513a878edd983d2b
Luiz Paulo S. Monteiro <35786917+adiffpirate@users.noreply.github.com>
2020-04-06 01:18:02 -0300

    Update convert+to_csv.py
    Some keys was missing when writing the csv file

--- tools/Convert JSON to CSV/convert+to_csv.py
+++ tools/Convert JSON to CSV/convert+to_csv.py
@@ -41,6 +41,7 @@ output_json = json.dump(programs, open(args.outfile_json, "w", encoding='utf8'),
 # Write CSV file from sorted JSON
 with open(args.outfile_csv, 'w', newline='') as file:
     writer = csv.writer(file)
-    writer.writerow(["program_name", "policy_url"])
+    writer.writerow(["program_name", "policy_url", "submission_url", "launch_date", "bug_bounty", "swag", "hall_of_fame", "safe_harbor"])
     for program in programs:
         writer.writerow([program['program_name'], program['policy_url']])
+        writer.writerow(["program_name", "policy_url", "submission_url", "launch_date", "bug_bounty", "swag", "hall_of_fame", "safe_harbor"])
