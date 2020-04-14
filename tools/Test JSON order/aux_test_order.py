6f3dbda78ce58a43e4740fe1f88de0b7e7cd4556
Luiz Paulo S. Monteiro <35786917+adiffpirate@users.noreply.github.com>
2020-04-14 18:07:47 -0300

    Update aux_test_order.py

--- tools/Test JSON order/aux_test_order.py
+++ tools/Test JSON order/aux_test_order.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-# Adaptation of convert+to_csv.py (github.com/disclose/disclose/tree/master/tools/Convert%20JSON%20to%20CSV)
+# Adaptation of convert_to_csv.py (github.com/disclose/disclose/tree/master/tools/Convert%20JSON%20to%20CSV)
 # removing the conversion part and leaving only the sorting and dumping of a json file.
 
 import argparse
@@ -8,28 +8,28 @@ import csv
 import json
 import sys
 from collections import OrderedDict
+import copy
 
 # Add commandline arguments.
 parser = argparse.ArgumentParser()
-required = parser.add_argument_group('Required Arguments')
-required.add_argument('-j', '--jsonfile', help='Input json file', type=str)
-required.add_argument('-oj', '--outfile-json', help='Output json file name', type=str)
+parser.add_argument('-j', '--jsonfile', default='program-list.json', help='Input json file', type=str)
+parser.add_argument('-oj', '--outfile-json', default='program-list.json', help='Output json file name', type=str)
 args = parser.parse_args()
-
-# Ensure proper arguments are given, and warn for missing ones.
-if None in (args.jsonfile, args.outfile_json):
-    if args.jsonfile is None: 
-        print("Error: input json file (-j/--jsonfile) is required")
-    if args.outfile_json is None:
-        print("Error: Output JSON filename (-oj/--outfile-json) is required")
-    sys.exit(0)
 
 # Open and load JSON file. Load as utf-8, due to non-ascii characters in some names.
 with open(args.jsonfile, 'r', encoding='utf-8') as f:
     programs = json.load(f, object_pairs_hook=OrderedDict)
 
-# Sort list alphabetically and dump to file.
+# Copy json.
+programs_sorted = copy.deepcopy(programs)
+# Sort alphabetically.
 # Converting key to lowercase is to ensure proper sorting, as capitals are given preference over lowercase.
 # This can lead to a situation such where the list goes Zynga Whitehat -> cPanel.
-programs.sort(key=lambda a: a['program_name'].lower())
-output_json = json.dump(programs, open(args.outfile_json, "w", encoding='utf8'), indent=2, ensure_ascii=False)
+programs_sorted.sort(key=lambda a: a['program_name'].lower())
+# Compare the original json with the sorted one and dump to file if they are different.
+if programs_sorted == programs:
+    print("File is sorted, everything is fine")
+else:
+    print("Uh-oh, some things are out of order")
+    output_json = json.dump(programs_sorted, open(args.outfile_json, "w", encoding='utf8'), indent=2, ensure_ascii=False)
+    print("Writed fixed json to " + args.outfile_json)
