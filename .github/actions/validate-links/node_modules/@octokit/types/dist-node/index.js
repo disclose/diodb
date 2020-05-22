475a85c72eaa3c0b002fd8d4923b53a006cf18d9
Ira Lun <sammyrosajoe@gmail.com>
2020-05-22 11:49:58 +0100

    Remove node_modules, package.json and package-lock.json.

--- .github/actions/validate-links/node_modules/@octokit/types/dist-node/index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, '__esModule', { value: true });
-
-const VERSION = "2.16.2";
-
-exports.VERSION = VERSION;
-//# sourceMappingURL=index.js.map
--- .github/actions/validate-links/node_modules/@octokit/types/dist-web/index.js
+++
@@ -1,4 +0,0 @@
-const VERSION = "2.16.2";
-
-export { VERSION };
-//# sourceMappingURL=index.js.map
