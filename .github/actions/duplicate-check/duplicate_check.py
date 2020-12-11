c223ef2a9c887280b94137d50939c37d84f469b2
prodigysml <sajeeb.lohani@bugcrowd.com>
2020-12-11 23:25:29 +1100

    Added in program name dup checking

--- .github/actions/duplicate-check/duplicate_check.py
+++ .github/actions/duplicate-check/duplicate_check.py
@@ -1,1 +1,25 @@
-print("running")
+import sys
+import json
+
+
+def duplicate_check_program_name(json_data):
+	program_names = list()
+
+	for program in json_data:
+		if program["program_name"] in program_names:
+			print("Duplicate program found. Name: " + program["program_name"])
+			return True
+		else:
+			program_names.append(program["program_name"])
+
+	return False
+
+
+if __name__ == "__main__":
+	full_path = sys.argv[1]
+
+	json_data = json.loads(open(full_path + "program-list.json").read())
+
+	if duplicate_check_program_name(json_data):
+		print("Duplicate programs found with the same name")
+		sys.exit(1)
