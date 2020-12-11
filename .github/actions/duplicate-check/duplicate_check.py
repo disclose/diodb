db7ef11a447686ef93d92ee6351b65d561eb0eda
prodigysml <sajeeb.lohani@bugcrowd.com>
2020-12-11 23:49:24 +1100

    Added in dup check for policy url

--- .github/actions/duplicate-check/duplicate_check.py
+++ .github/actions/duplicate-check/duplicate_check.py
@@ -2,24 +2,34 @@ import sys
 import json
 
 
-def duplicate_check_program_name(json_data):
-	program_names = list()
+def duplicate_check_boilerplate(json_data, param, context):
+	values = list()
 	ret_val = False
 
 	for program in json_data:
-		if program["program_name"] in program_names:
-			print("Duplicate program found. Name: " + program["program_name"])
+		if program[param] in values:
+			print("Duplicate " + context + " found. " + context + ": " + program[param])
 			ret_val = True
 		else:
-			program_names.append(program["program_name"])
+			program_names.append(program[context])
 
 	return ret_val
+
+
+def duplicate_check_policy_url(json_data):
+	return duplicate_check_boilerplate(json_data, "policy_url", "policy url")
+
+
+def duplicate_check_program_name(json_data):
+	return duplicate_check_boilerplate(json_data, "program_name", "name")
 
 
 if __name__ == "__main__":
 	full_path = sys.argv[1]
 
 	json_data = json.loads(open(full_path + "program-list.json").read())
+
+	duplicate_check_policy_url(json_data)
 
 	if duplicate_check_program_name(json_data):
 		print("Duplicate programs found with the same name")
