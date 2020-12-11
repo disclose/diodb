2bf2eebb4686768173b5808224b33aca0624268c
prodigysml <sajeeb.lohani@bugcrowd.com>
2020-12-11 23:30:45 +1100

    Fixed stopping at one issue for program name dup checking

--- .github/actions/duplicate-check/duplicate_check.py
+++ .github/actions/duplicate-check/duplicate_check.py
@@ -4,15 +4,16 @@ import json
 
 def duplicate_check_program_name(json_data):
 	program_names = list()
+	ret_val = False
 
 	for program in json_data:
 		if program["program_name"] in program_names:
 			print("Duplicate program found. Name: " + program["program_name"])
-			return True
+			ret_val = True
 		else:
 			program_names.append(program["program_name"])
 
-	return False
+	return ret_val
 
 
 if __name__ == "__main__":
@@ -23,3 +24,5 @@ if __name__ == "__main__":
 	if duplicate_check_program_name(json_data):
 		print("Duplicate programs found with the same name")
 		sys.exit(1)
+
+
